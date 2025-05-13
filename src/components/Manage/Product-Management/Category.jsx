fba3e787420c6ed97b1d6d944188517366983506
Robinsrk07 <robinsyriak07@gmail.com>
2025-05-13 12:22:52 +0530

    table width issue

--- src/components/Manage/Product-Management/Category.jsx
+++ src/components/Manage/Product-Management/Category.jsx
@@ -177,7 +177,7 @@ import '@fontsource/open-sans/700.css'; // Bold
                  
                        
                  
-                       <table className="table w-full text-sm text-left text-gray-500 border-collapse   " style={{ borderSpacing: '0 12px', borderCollapse: 'separate', }}>
+                       <table className="table w-full text-sm text-left text-gray-500 border-collapse  min-w-1200px  " style={{ borderSpacing: '0 12px', borderCollapse: 'separate',minWidth: '1200px' }}>
                          <thead className="text-xs text-gray-400 uppercase bg-white">
                            <tr>
                            <th className="px-6 py-3" style={{ width: '100px', paddingLeft: '20px' }}>SL NO</th>
